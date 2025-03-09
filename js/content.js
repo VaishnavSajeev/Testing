7e8142e09a28a2ebacb9e17eb1c340cb481747ec
Vaishnav200517918 <116231665+Vaishnav200517918@users.noreply.github.com>
2025-03-09 14:04:19 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -9,7 +9,7 @@ function renderContentList(contentList, containerId) {
     const cardHTML = `
         <div class="col-md-4 mb-4">
           <div class="card h-100" onclick="location.href='details.html?id=${content.id}'" style="cursor: pointer;">
-            <img src="${content.image}" class="card-img-top" alt="${content.title}">
+            <img src="${content.image}" class="card-img-top ratio ratio-4x3" alt="${content.title}">
             <div class="card-body">
               <h5 class="card-title">${content.title}</h5>
               <p class="card-text">${content.shortDescription}</p>
